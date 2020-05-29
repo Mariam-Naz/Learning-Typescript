b519ac0bfb7d078c1c04b5a8ebc040556d291807
unknown <mariamnaz212@gmail.com>
2020-05-30 04:06:13 +0500

    working with interface

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
 
-enum color  {Red =0, Blue=1, Green=3 };
-let backgeound = color.Blue;
+// this is not a good approach xxx
+// let drawPoint = (point:{x:number, y:number}) => {
+//     //...
+// }
+
+interface Point{
+    x:number,
+    y:number
+}
+let drawPoint = (point:Point) => {
+        //...
+}
+drawPoint({
+    x:9,
+    y:10
+});
+
+
