e8fa0cef8464cac9ce67b4ab997ddc9aff5beb6a
unknown <mariamnaz212@gmail.com>
2020-05-29 19:06:00 +0500

    learning type-script

---
+++ main.js
@@ -0,0 +1,8 @@
+var color;
+(function (color) {
+    color[color["Red"] = 0] = "Red";
+    color[color["Blue"] = 1] = "Blue";
+    color[color["Green"] = 3] = "Green";
+})(color || (color = {}));
+;
+var backgeound = color.Blue;
---
+++ main.ts
@@ -0,0 +1,3 @@
+
+enum color  {Red =0, Blue=1, Green=3 };
+let backgeound = color.Blue;
