05ddb136ddc2895a6768b0095098831df5b2f1e1
unknown <mariamnaz212@gmail.com>
2020-06-02 05:36:00 +0500

    working with set get methods in class

--- main.js
+++ main.js
@@ -6,10 +6,17 @@ var Point = /** @class */ (function () {
     Point.prototype.draw = function () {
         console.log('X: ' + this.x + ', Y: ' + this.y);
     };
-    Point.prototype.getDistance = function (another) {
-        //..
+    Point.prototype.getX = function () {
+        return this.x;
+    };
+    Point.prototype.setX = function (value) {
+        if (value > 10)
+            throw new Error('value must be less than 10');
+        this.x = value;
     };
     return Point;
 }());
-var point = new Point();
+var point = new Point(3, 4);
+point.setX(5);
+point.getX();
 point.draw();
--- main.ts
+++ main.ts
@@ -1,20 +1,24 @@
 
 class Point{
-   private x:number; //access modifier
-    private y:number;
-
-    constructor(x?:number , y?:number){ // ? is for optional
-        this.x = x;
-        this.y = y;
+    constructor(private x?:number ,  private y?:number){ // ? is for optional , // an especial way to do private and it clean our code
     }
     draw(){
         console.log('X: ' + this.x +', Y: ' + this.y);
+    }
+    getX(){
+        return this.x
+    }
+    setX(value){
+        if(value>10)
+            throw new Error('value must be less than 10');
+        this.x = value;
     }
 
 }
 
 let point= new Point(3,4);
-
+point.setX(5);
+point.getX();
 point.draw();
 
 
